e6512012cec45ef0eca6b28a03c18e6c74949bbf
League <league@Mac-mini.local>
2018-04-28 12:22:30 -0700

    Created a scrolling ground
    + made a single scrolling ground
    - the array list I made to add more grounds to the list isn't doing... that thing.

--- src/dragonRunner/Ground.java
+++ src/dragonRunner/Ground.java
@@ -1,10 +1,24 @@
 package dragonRunner;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Ground extends GameObject {
+	private int groundMoveSpeed;
 
 	public Ground(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
+		groundMoveSpeed = 4;
 	}
-	private int groundMoveSpeed;
+	
+	public void update() {
+		x = x - groundMoveSpeed;
+		super.update();
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y, width, height);
+	}
+
 }
--- src/dragonRunner/ObjectManagerK.java
+++ src/dragonRunner/ObjectManagerK.java
@@ -5,34 +5,57 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
 
+import org.jointheleague.leagueinvaders.Rocketship;
+
 public class ObjectManagerK {
+	
+//THE CLOUDS:
 	private long lastCloudCreated = 0;
 	private int cloudGeneratorPause = 3000;
-	
+
 	// Making the Cloud List
-	ArrayList <Cloud> cloudList = new ArrayList <>();
-	
-	public void addCloudToList (Cloud cloud){
+	ArrayList<Cloud> cloudList = new ArrayList<>();
+
+	public void addCloudToList(Cloud cloud) {
 		cloudList.add(cloud);
 	}
-	
-	//Randomly generating clouds
+
+	// Randomly generating clouds
 	public void generateClouds() {
 		if (System.currentTimeMillis() - lastCloudCreated >= cloudGeneratorPause) {
-			addCloudToList(new Cloud(dragonRunnerMain.FRAME_WIDTH, new Random().nextInt(dragonRunnerMain.FRAME_HEIGHT / 2), 60, 40));
+			addCloudToList(new Cloud(dragonRunnerMain.FRAME_WIDTH,
+					new Random().nextInt(dragonRunnerMain.FRAME_HEIGHT / 2), 60, 40));
 			lastCloudCreated = System.currentTimeMillis();
 		}
 	}
+//THE GROUND:
+	Ground firstGround = new Ground(200, dragonRunnerMain.FRAME_HEIGHT - 150, 300, dragonRunnerMain.FRAME_HEIGHT);
 	
-	public void update(){
+	ArrayList<Ground> groundList = new ArrayList<>();
+	
+	public void addGroundToList(Ground ground) {
+		groundList.add(ground);
+	}
+	
+	public void loopGround() {
+		if(firstGround.x + firstGround.width >= 400) {
+			System.out.println("adding a ground");
+			addGroundToList(new Ground(firstGround.x + firstGround.width, dragonRunnerMain.FRAME_HEIGHT - 150, 300, dragonRunnerMain.FRAME_HEIGHT));
+		}
+	}
+
+//UPDATE AND DRAW:
+	public void update() {
+		firstGround.update();
 		generateClouds();
-		for(int i = 0; i < cloudList.size(); i++){
+		for (int i = 0; i < cloudList.size(); i++) {
 			cloudList.get(i).update();
 		}
 	}
-	
-	public void draw(Graphics g){
-		for(int i = 0; i < cloudList.size(); i++){
+
+	public void draw(Graphics g) {
+		firstGround.draw(g);
+		for (int i = 0; i < cloudList.size(); i++) {
 			cloudList.get(i).draw(g);
 		}
 	}
