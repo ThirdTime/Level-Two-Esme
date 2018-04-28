4951eb557baa02b04c242ce4067df39f263bd5e2
League <league@Mac-mini.local>
2018-04-28 10:44:55 -0700

    Made a ground class

---
+++ src/dragonRunner/Ground.java
@@ -0,0 +1,10 @@
+package dragonRunner;
+
+public class Ground extends GameObject {
+
+	public Ground(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+	private int groundMoveSpeed;
+}
