9d0d7967fb13973128f700e09f0351da9c29b27e
angelvillarreal1 <league@Mac-mini-4.local>
2018-05-12 12:14:45 -0700

    Made arrow objects, did a small amount of clean-up

---
+++ src/dragonRunner/toDoToday.java
@@ -0,0 +1,14 @@
+package dragonRunner;
+
+public class toDoToday {
+	//What do you need to do today?
+	
+	/* Make an isAlive checker for the clouds, ground and arrows (and the spikes, eventually)
+	 * If you're done with that, try making a collision checker, but that might be pointless since you haven't made a dragon.
+	 * Or try making a dragon! It's up to you.
+	 */
+	
+	
+	
+	
+}
