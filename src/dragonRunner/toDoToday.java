6ef8feddbe30827b78c147aa74f6a67ac47662ac
ThirdTime <league@WorthyMini.local>
2018-05-19 12:14:45 -0700

    To do today

--- src/dragonRunner/toDoToday.java
+++ src/dragonRunner/toDoToday.java
@@ -3,9 +3,10 @@ package dragonRunner;
 public class toDoToday {
 	//What do you need to do today?
 	
-	/* Make an isAlive checker for the clouds, ground and arrows (and the spikes, eventually)
-	 * If you're done with that, try making a collision checker, but that might be pointless since you haven't made a dragon.
-	 * Or try making a dragon! It's up to you.
+	/* Figure out how to make the spikes different sizes
+	 * if needed, make subclasses of the spikes class with different sizes
+	 * that's pretty inconvenient, though.
+	 * just work on spikes overall, I guess
 	 */
 	
 	
